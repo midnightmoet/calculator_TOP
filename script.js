2532e4cd185c8f0c2f242e157ab98b2db9289f28
Lisa Miner <midnightmoet@gmail.com>
2023-01-19 15:32:42 -0500

    added dark/light and some nicer looks

---
+++ script.js
@@ -0,0 +1,32 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+// this enables the calculator to perform the basic operations
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    } else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Empty!";
+      setTimeout(() => (display.innerText = ""), 2000);
+    } else {
+      display.innerText += item.id;
+    }
+  };
+});
+
+//  this enables the toggle button to change the theme of the calculator
+const themeToggleBtn = document.querySelector(".theme-toggle");
+const calculator = document.querySelector(".calculator");
+const toggleIcon = document.querySelector(".toggle-icon");
+let isDark = true;
+themeToggleBtn.onclick = () => {
+  calculator.classList.toggle("dark");
+  themeToggleBtn.classList.toggle("active");
+  isDark = !isDark;
+};
